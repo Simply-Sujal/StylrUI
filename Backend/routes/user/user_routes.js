ccc98aad603edcfa2a2da4160223bd69fa8ca74a
Sujal <sujal.kumar311@gmail.com>
2024-06-02 22:43:02 +0530

    Introduce auth middleware , for protecting the some important route

--- Backend/routes/user/user_routes.js
+++ Backend/routes/user/user_routes.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { loginUser, registerUser } from "../../controllers/user/user_controllers.js";
+import authMiddleware from "../../middlewares/auth_middlewares.js";
 
 const router = express.Router();
 
@@ -8,5 +9,13 @@ router.post("/register", registerUser)
 
 // this is the login user route
 router.post("/login", loginUser)
+
+// protected route
+router.get("/protected", authMiddleware, (req, res) => {
+    res.json({
+        success: true,
+        message: "This is from the protected route"
+    })
+})
 
 export default router;
